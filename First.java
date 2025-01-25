3649e955b6d093ad15de4d47a3f855d960d6f9ad
pranav1237 <pranavmahajan.4122005@gmail.com>
2025-01-25 11:31:27 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,30 @@
+
+public class First{
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Please enter some input: ");
+        // int rollno = sc.nextInt();
+        // System.out.println("Your roll number is: "+rollno);
+
+        // byte a = 40;
+        // byte b= 50;
+        // byte c = 100;
+        // int d =(a*b)/c; 
+        // System.out.println(d);
+
+        // int number = 'A';
+        // System.out.println(number);
+
+
+        byte b =42;
+        char c= 'a';
+        short s = 1024;
+        int i = 500000;
+        float f = 5.67f;
+        double d= 0.12443;
+        double result = (f*b)+(i/c)-(d-s);
+        System.out.println( (f*b)+" "+(i/c)+" "+(d-s));
+        System.out.println(result);
+
+    }
+}
---
+++ loop.java
@@ -0,0 +1,64 @@
+public class loop{
+    public static void main(String[] args) {
+        // int a = 10;
+        // if (a==10){
+        //     System.out.println("Hello World!");
+       // }
+
+        // while loop:
+    //    int count = 1;
+    //    while(count !=5){
+    //     System.out.println(count);
+    //     count++;
+    //    }
+
+        // for loop:
+
+        // for(int count = 1; count !=5; count++){
+        //     System.err.println(count);
+        // }
+
+
+        /* Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter temp in C: ");
+        float tempC= sc.nextFloat();
+        float tempF = (tempC*9/5) +32;
+        System.out.println(tempF); */
+
+        // if looop:
+        
+       // Scanner sc = new Scanner(System.in);
+        // int salary = sc.nextInt();
+        // if (salary>10000){
+        //     salary = salary+2000;
+
+        // }else{
+        //     salary = salary+1000;
+        // }
+        // System.err.println(salary);
+
+      /*   int num=1;
+        while(num<=5){
+            System.out.println(num);
+            num++;
+        } */
+
+        // for (int num = 1; num<=10; num+=2){
+        //     System.out.println(num);
+        // }
+        
+        
+      /*  Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        sc.close(); // Close the scanner to avoid resource leak
+        System.out.println("Printing odd numbers up to your number:");
+        
+        for (int i = 1; i <= num; i += 2) {
+            System.out.println(i);
+        } */
+
+        
+
+    }
+}
